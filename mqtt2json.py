a457fad496a97da187e065352be7cb039aa8e914
Stefan Bethke <stb@lassitu.de>
2023-07-14 16:21:54 +0200

    Add stations to map

--- mqtt2json.py
+++ mqtt2json.py
@@ -172,7 +172,7 @@ class MqttClient():
         logging.info("Reconnect failed after %s attempts. Exiting...", reconnect_count)
 
 
-for f in ('index.html', 'lok.png'):
+for f in ('index.html', 'lok.png', 'station.png'):
     copy(f'webroot/{f}', f'data/{f}')
 
 tracks = gpx2tracks()
