c7e0283eb2ba1406481ee7ce9e207b78e3e39411
Stefan Bethke <stb@lassitu.de>
2023-08-07 22:36:39 +0200

    Use MQTTS

--- arduino/c3toc_traintracker/c3toc_traintracker.ino
+++ arduino/c3toc_traintracker/c3toc_traintracker.ino
@@ -1,8 +1,8 @@
-#include <ArduinoMqttClient.h>
 #include <CoopTask.h>
 #include <EEPROM.h>
+#include <PubSubClient.h>
 #include <TinyGPS++.h>
-#include <WiFi.h>
+#include <WiFiClientSecure.h>
 #include <WiFiManager.h>
 
 #include <XPowersLib.h>
@@ -14,8 +14,10 @@ TinyGPSPlus gps;
 //XPowersPMU PMU;
 XPowersLibInterface *PMU = NULL;
 
-WiFiClient wifiClient;
-MqttClient mqttClient(wifiClient);
+// WiFiClient wifiClient;
+WiFiClientSecure wifiClient;
+// MqttClient mqttClient(wifiClient);
+PubSubClient pubSubClient(wifiClient);
 
 struct mqttParam {
   char valid;
@@ -26,7 +28,40 @@ struct mqttParam {
 
 int wifiParamsUpdated = 0;
 
-String topic = "c3toc/demo";
+char topic[] = "c3toc/demo";
+
+const char* root_ca= \
+     "-----BEGIN CERTIFICATE-----\n" \
+     "MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n" \
+     "TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
+     "cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n" \
+     "WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n" \
+     "ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n" \
+     "MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n" \
+     "h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n" \
+     "0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n" \
+     "A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n" \
+     "T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n" \
+     "B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n" \
+     "B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n" \
+     "KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n" \
+     "OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n" \
+     "jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n" \
+     "qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n" \
+     "rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n" \
+     "HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n" \
+     "hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n" \
+     "ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n" \
+     "3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n" \
+     "NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n" \
+     "ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n" \
+     "TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n" \
+     "jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n" \
+     "oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n" \
+     "4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n" \
+     "mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n" \
+     "emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n" \
+     "-----END CERTIFICATE-----\n";
 
 
 void loadMqttParam() {
@@ -38,6 +73,7 @@ void loadMqttParam() {
   EEPROM.end();
 }
 
+
 void saveMqttParam() {
   EEPROM.begin(sizeof(mqttParam));
   char *p = (char *)&mqttParam;
@@ -47,6 +83,7 @@ void saveMqttParam() {
   EEPROM.commit();
   EEPROM.end();
 }
+
 
 void gpsDebug() {
   Serial.print("chars: ");
@@ -84,64 +121,59 @@ void heartbeat() {
 }
 
 
-void mqttLastWill() {
-  String willPayload = "offline";
-  mqttClient.beginWill(topic + "/" + mqttParam.user + "/status", willPayload.length(), true, 1);
-  mqttClient.print(willPayload);
-  mqttClient.endWill();
-  Serial.println("Updated last will");
+char *format_topic(const char *t, const char *u, const char *i) {
+  static char s[64];
+  snprintf(s, sizeof(s), "%s/%s/%s", t, u, i);
+  return s;
 }
 
-void mqttAlive() {
-  mqttClient.setId(mqttParam.user);
-  mqttClient.setUsernamePassword(mqttParam.user, mqttParam.pass);
-  mqttLastWill();
+
+void mqttConnect() {
+  while (!pubSubClient.connected()) {
+    Serial.println("Attempting to connect to the MQTT server");
+    while (!pubSubClient.connect(mqttParam.user, mqttParam.user, mqttParam.pass,
+      format_topic(topic, "mqttParam.user", "status"), 1, true, "offline")) {
+        Serial.print("Connection to MQTT server failed: ");
+        Serial.println(pubSubClient.state());
+        delay(5000);
+    }
+    Serial.println("Connected to the MQTT server");
+  }
+}
+
+
+void mqttUpdate() {
+  char payload[512];
+  char ts[64];
+  time_t now = time(nullptr);
+  struct tm timeinfo;
 
   for (;;) {
-    while (!mqttClient.connected()) {
-      Serial.print("Attempting to connect to the MQTT broker: \"");
-      Serial.print(mqttParam.host);
-      Serial.print("\"\r\nuser: \"");
-      Serial.print(mqttParam.user);
-      Serial.print("\"\r\npass: \"");
-      Serial.print(mqttParam.pass);
-      Serial.print("\"\r\n");
-
-      if (mqttClient.connect(mqttParam.host, 1883)) {
-        Serial.println("You're connected to the MQTT broker!");
-      } else {
-        Serial.print("MQTT connection failed! Error code = ");
-        Serial.println(mqttClient.connectError());
-        delay(10000);
-      }
+    if (!pubSubClient.connected()) {
+      delay(1000);
+      continue;
     }
+    now = time(nullptr);
+    gmtime_r(&now, &timeinfo);
+    strftime(ts, sizeof(ts), "%Y-%m-%dT%H:%M:%SZ", &timeinfo);
 
-    mqttClient.beginMessage(topic + "/" + mqttParam.user + "/status");
-    mqttClient.print("alive");
-    mqttClient.endMessage();
+    snprintf(payload, sizeof(payload), "alive %s", ts);
+    pubSubClient.publish(format_topic(topic, "mqttParam.user", "status"), payload);
 
-    mqttClient.beginMessage(topic + "/" + mqttParam.user + "/pos");
-    mqttClient.print("{\"lat\":");
-    mqttClient.print(gps.location.lat(),5);
-    mqttClient.print(",\"lon\":");
-    mqttClient.print(gps.location.lng(),5);
-    mqttClient.print(",\"sat\":");
-    mqttClient.print(gps.satellites.value());
-    mqttClient.print(",\"speed\":");
-    mqttClient.print(gps.speed.kmph());
-    mqttClient.print(",\"Vbat\":");
-    mqttClient.print(PMU->getBattVoltage());
-    mqttClient.print(",\"Vbus\":");
-    mqttClient.print(PMU->getVbusVoltage());
-    mqttClient.print(",\"Vsys\":");
-    mqttClient.print(PMU->getSystemVoltage());
-    mqttClient.print("}");
-    mqttClient.endMessage();
-    Serial.println("Sent MQTT messages");
+    snprintf(payload, sizeof(payload), "{\"lat\":%.5f,\"lon\":%.5f,\"sat\":%d,\"speed\":%.1f,\"Vbat\":%.2f,\"Vbus\":%.2f,\"ts\":\"%s\"}",
+      gps.location.lat(),
+      gps.location.lng(),
+      gps.satellites.value(),
+      gps.speed.kmph(),
+      PMU->getBattVoltage()/1000.0,
+      PMU->getVbusVoltage()/1000.0,
+      ts);
+    pubSubClient.publish(format_topic(topic, "mqttParam.user", "pos"), payload);
 
     delay(10000);
   }
 }
+
 
 void reconfigure() {
   pinMode(BUTTON, INPUT);
@@ -158,27 +190,43 @@ void reconfigure() {
 }
 
 
-void power() {
-  while(1) {
-    uint32_t status = PMU->getIrqStatus();
-    // Serial.print("STATUS => HEX:");
-    // Serial.print(status, HEX);
-    // Serial.print(",\"Vbat\":");
-    // Serial.println(status, BIN);
-    PMU->clearIrqStatus();
-    delay(1000);
-  }
+void gpsAlive() {
+  while (Serial1.available())
+    gps.encode(Serial1.read());
+  delay(10);
 }
 
+
 BasicCoopTask<CoopTaskStackAllocatorAsMember<2000>> heartbeatTask("heartbeat", heartbeat);
-BasicCoopTask<CoopTaskStackAllocatorAsMember<2000>> mqttAliveTask("mqttAlive", mqttAlive);
+BasicCoopTask<CoopTaskStackAllocatorAsMember<16384>> mqttUpdateTask("mqttUpdate", mqttUpdate);
 BasicCoopTask<CoopTaskStackAllocatorAsMember<2000>> reconfigureTask("reconfigure", reconfigure);
-BasicCoopTask<CoopTaskStackAllocatorAsMember<2000>> powerTask("power", power);
+BasicCoopTask<CoopTaskStackAllocatorAsMember<2000>> gpsTask("gps", gpsAlive);
+
+
+
+
+void setClock() {
+  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+
+  Serial.print("Waiting for NTP time sync: ");
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("");
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time: ");
+  Serial.print(asctime(&timeinfo));
+}
 
 
 void wifiParamsUpdatedCallback() {
   wifiParamsUpdated = 1;
 }
+
 
 void setupWifi() { 
   int p = 0;
@@ -231,6 +279,8 @@ void setupWifi() {
     Serial.print(mqttParam.pass);
     Serial.print("\"\r\n");
   }
+
+  setClock();
 }
 
 
@@ -322,6 +372,7 @@ void setupPower() {
   // PMU->fuelGaugeControl(true, true);
 }
 
+
 void setup() {
   Serial.begin(230400);
   Serial.println("\n\rc3toc train tracker booting...\n\r");
@@ -337,13 +388,16 @@ void setup() {
 
   heartbeatTask.scheduleTask();
   reconfigureTask.scheduleTask();
-  mqttAliveTask.scheduleTask();
-  powerTask.scheduleTask();
+  mqttUpdateTask.scheduleTask();
+  gpsTask.scheduleTask();
+
+  wifiClient.setInsecure();
+  pubSubClient.setServer(mqttParam.host, 8883);
 }
 
 void loop() {
   runCoopTasks();
-  mqttClient.poll();
-  while (Serial1.available())
-    gps.encode(Serial1.read());
+  mqttConnect();
+  pubSubClient.loop();
+  delay(10);
 }
